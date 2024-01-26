7f77017bded87d4db928e6b6d03d445ecc1f8bb2
bochen0116 <154125836+bochen0116@users.noreply.github.com>
2024-01-27 00:22:42 +0800

    Create Userinfo.sql

---
+++ Userinfo.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Userinfo (
+    userid INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    uAccountEmail NVARCHAR(30) NOT NULL UNIQUE,
+    uPwd NVARCHAR(20) NOT NULL,
+    uName NVARCHAR(25) NOT NULL,
+    uNickname NVARCHAR(10),
+    uGender CHAR(1),
+    uBirthday DATE,
+    uTel NVARCHAR(25) NOT NULL,
+    addCityid INT,
+    addAreaid INT,
+    uIdCode CHAR(10),
+    roleId INT,
+    userImgid INT --REFERENCES UserImg(userImgid),
+);
