b50190d0b2c13c0bc35c264855b6d05c2b983551
bochennnnn <bochen08601@gmail.com>
2024-02-01 23:14:47 +0800

    增加 UserImg.sql 的輸入資料

--- UserImg.sql
+++ UserImg.sql
@@ -3,3 +3,15 @@ CREATE TABLE UserImg(
     userId INT NOT NULL,
     uImgpath NVARCHAR(255) NOT NULL,
 )
+
+INSERT INTO UserImg(userid,uImgpath) 
+VALUES (1,'images/icon/avatar-01.jpg'),
+(2,'images/icon/avatar-02.jpg'),
+(3,'images/icon/avatar-01.jpg'),
+(4,'images/icon/avatar-03.jpg'),
+(5,'images/icon/avatar-04.jpg'),
+(6,'images/icon/avatar-05.jpg'),
+(7,'images/icon/avatar-06.jpg'),
+(8,'images/icon/avatar-01.jpg'),
+(9,'images/icon/avatar-02.jpg'),
+(10,'images/icon/avatar-03.jpg')
